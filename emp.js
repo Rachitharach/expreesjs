8875bf77662b7d306819352dfde6c8bba4f28fd1
Rachitharach <rachitharach06@gmail.com>
2026-06-28 22:59:19 +0530

    Employe crud with loginsecurity

--- emp.js
+++ emp.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+
+const SECRET_KEY = 'mysecretkey';
 
 const app=express();
 app.use(express.json());
+
 
 mongoose.connect("mongodb://localhost:27017/empinfo")
 .then(()=>{
@@ -27,16 +32,29 @@ const empSchema = new mongoose.Schema({
 const employe = mongoose.model("Employe",empSchema);
 employe.find()
 
-// const userSchema = new mongoose.Schema({
-//     username:String,
-//     email:String,
-//     password:String
-// });
-// const user = mongoose.model("user",userSchema);
+const authentication = (req,res,next)=>{
+    const token = req.headers.authorization;
+    //console.log(token);
+    if(!token){
+        return res.status(401).send("Token required");
+    }
+   
+    const jwtToken = token.split(" ")[1];
+
+    jwt.verify(jwtToken,SECRET_KEY,(error,decoded)=>{
+        if(error){
+            return res.status(401).send("ivalid token")
+        }
+        console.log(decoded);
+        req.user = decoded;
+        next();  
+    });
+    
+
+};
 
 
-
-app.post('/employe', (req, res) => {
+app.post('/employe',(req, res) => {
     bcrypt.hash(req.body.password, 10)
     .then((hash) => {
     req.body.password = hash;
@@ -54,33 +72,7 @@ app.post('/employe', (req, res) => {
     });
 });
 
-app.get('/employe',(req,res)=>{
-    employe.find()
-    .then((data) => {
-    res.status(200).send(data);
-    })
-    .catch((err) => {
-        res.status(500).send(err);
-    });
-})
-
-app.delete('/employe', (req, res) => {
-    employe.deleteOne({
-        _id: "6a3e5fb23ab4690823ba42fe"
-    })
-    .then((data) => {
-    res.send(data);
-    })
-    .catch((err) => {
-    res.status(500).send(err);
-    });
-
-});
-
-
-
 app.post('/login',(req,res)=>{
-    
     employe.findOne({
         email:req.body.email
     })
@@ -88,11 +80,19 @@ app.post('/login',(req,res)=>{
         if(!user){
             return res.send("User Not Found");
         }
-        
-        bcrypt.compare(req.body.password,user.password)
+    bcrypt.compare(req.body.password,user.password)
         .then((result)=>{
             if(result){
-                return res.send("Login Successful");
+                const token = jwt.sign(
+                    {
+                        email:user.email
+                    },
+                    SECRET_KEY
+                );
+                return res.send({
+                    message:"Login Successful",
+                    token: token
+            });
             }else{
                 return res.send("Invalid Password");
             }
@@ -107,6 +107,132 @@ app.post('/login',(req,res)=>{
     })
 })
 
-app.listen(5000,()=>{
-    console.log("server running");
+
+
+app.get('/employe',(req,res)=>{
+    employe.find()
+    .then((data) => {
+    res.status(200).send(data);
+    })
+})
+
+app.get('/employeauthe',authentication,(req,res)=>{
+    employe.find()
+    .then((data) => {
+    res.status(200).send(data);
+    })
+    .catch((err) => {
+        res.status(500).send(err);
+    });
+})
+
+
+
+
+app.get("/employehead", (req, res) => {
+    console.log(req.headers);//terminal
+    res.send("employe");//browser
+});
+
+app.get('/employesort',(req,res)=>{
+    employe.find()
+    .sort({name:1})
+    .then((data)=>{
+        res.status(200).send(data);
+    })
+    .catch((error)=>{
+        res.status.send(error)
+    })
+})
+
+app.get('/employes',(req,res)=>{
+    employe.find()
+    .limit(2)
+    .then((data)=>{
+    res.status(200).send(data);
+    })
+    .catch((error)=>{
+        res.status(500).send(error)
+    })
+})
+
+
+
+
+
+app.put('/employeupdate/:id',(req,res)=>{
+    employe.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new:true}
+    )
+    .then((data)=>{
+        res.status(200).send(data)
+    })
+    .catch((error)=>{
+        res.status(500).send(error)
+    })
+})
+
+app.patch('/employepatch/:id',(req,res)=>{
+    employe.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new:true}
+    )
+    .then((data)=>{
+        res.status(200).send(data)
+    })
+    .catch((error)=>{
+        res.status(500).send(error)
+    })
+})
+
+
+app.delete('/employe/:id', (req, res) => {
+    employe.findByIdAndDelete(req.params.id)
+    .then((data) => {
+    res.send(data);
+    })
+    .catch((err) => {
+    res.status(500).send(err);
+    });
+
+});
+
+
+app.get('/employedep',(req,res)=>{
+    employe.find({department:req.query.department})
+    .then((data)=>{
+        res.send(data)
+    })
+    .catch((err)=>{
+        res.send(err)
+    })
+})
+
+
+app.get('/employeagg',(req,res)=>{
+    employe.aggregate([
+        {
+            $group:{
+                _id:null,
+                totalsalary:{
+                    $sum:"$salary"
+                }
+            }
+        }
+    ])
+    .then((data)=>{
+        res.send(data)
+    })
+    .catch((err)=>{
+        res.send(err)
+    })
+})
+
+
+
+app.listen(8000,()=>{
+    console.log("server is running now..")
 })
